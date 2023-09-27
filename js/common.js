60a176bac1bf6efef307e0c73ca96fd5d916f295
Serge Goncharov <sgonch@hotmail.com>
2023-09-27 17:29:57 -0400

    New Script

--- js/common.js
+++ js/common.js
@@ -1,31 +1,34 @@
 function header () {
   with(document) {
        write("<div id='headerMain'>");
-           write("<div class='logo'>Logo")
-           write("</div>");
+            write("<a class='logo' href='./index.html'>");
+              write("<span class='nav-title'>AIAI</span>");
+            write("</a>");
            write("<div class='menuMain'>");
                write("<ul class='menu'>");
-                   write("<li><a href='include.html' id='rgtlink1' name='rgtlink1'>Home</a>");
-                   write("</li>")
-                   write("<li><a href='product.html' id='rgtlink2' name='rgtlink2'  onmouseover='show(\"t1\",\"rgtlink2\")' onmouseout='hide(\"t1\",\"rgtlink2\")'>Products</a>");
-                     write("<div style='display:none;' id='t1' onmouseover='show(\"t1\",\"rgtlink2\")' onmouseout='hide(\"t1\",\"rgtlink2\")' class='sublnkBox'>");
-                          write("<ul>");
-                              write("<li><a href='product_1.html' id='rgtlink01' name='rgtlink01'>Product1</a></li>");
-                              write("<li><a href='product_2.html' id='a02' name='a02'>Product2</a></li>");
-                              write("<li><a href='#'>Product3</a></li>");
-                              write("<li><a href='#'>Product4</a></li>");
-                              write("<li><a href='#'>Product5</a></li>");
-                          write("</ul>");
-                      write("</div>");
-                   write("</li>")
-                   write("<li><a href='about.html' id='a1' name='a1'>About Us</a>");
-
-                   write("</li>")
-                   write("<li><a href='contact.html' id='rgtlink3' name='rgtlink3'>Contact Us</a>");
-                   write("</li>")
+                    write("<li><a href='index.html' id='rgtlink1' name='rgtlink1'>Home</a>");
+                    write("</li>")
+                    write("<li><a href='call.html' id='a1' name='a1'>Call for papers</a>");
+                    write("</li>")
+                    write("<li><a href='program.html' id='rgtlink3' name='rgtlink3'>Program</a>");
+                    write("</li>")
+                    write("<li><a href='practical_info.html' id='rgtlink3' name='rgtlink3'>Practical Information</a>");
+                    write("</li>")
+                    write("<li><a href='mailto:julie.goncharov@uni-goettingen.de' id='rgtlink3' name='rgtlink3'>Contact us</a>");
+                    write("</li>")
                write("</ul>");
            write("</div>");
-   write("</div>");
+        write("</div>");
+      write("<div id='project-header'>");
+            write("<image class='project-header-image' src='./assets/images/AIAI_prog.png'/>");
+                write("<div class='main-title'>Agency and Intentions in AI</div>");
+                  write("<div>");
+                      write("<blockquote><p>How do you connect with a single piece of information?</p><p>Date ‘em</p>");
+                          write("<footer><cite>Kyle Thompson</cite></footer>");
+                      write("</blockquote>");
+                write("</div>");
+            write("</div>");
+      write("</div>");
   }
   imgsearch();
 }
